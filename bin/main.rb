9b825c3bf218635bd2563096a979e0fcfadd48eb
Simon Peter Debbarma <31207418+simonpeterdebbarma@users.noreply.github.com>
2020-02-07 11:11:25 -0600

    :rotating_light: Fix lint warnings on validation, layout, and style
    rubocop -a did most of the work; you can also use name != ' ' to test a second false condition

--- bin/main.rb
+++ bin/main.rb
@@ -2,18 +2,18 @@
 
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 def display_board(board)
-  puts " #{board[0]}| #{board[1]} |#{board[2]}"
-  puts '---------'
-  puts " #{board[3]}| #{board[4]} |#{board[5]}"
-  puts '---------'
-  puts " #{board[6]}| #{board[7]} |#{board[8]}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts '-----------'
+  puts " #{board[3]} | #{board[4]} | #{board[5]}"
+  puts '-----------'
+  puts " #{board[6]} | #{board[7]} | #{board[8]}"
 end
 
 def validate_name(name)
-  if name == '' || name == ' '
+  if name == ''
     false
   else
-    true
+    name != ' '
   end
 end
 
@@ -21,38 +21,38 @@ display_board(board)
 
 puts 'Welcome to Tic-Tac-Toe!'
 puts 'Player 1 - Please enter your name:'
-player_1 = gets.chomp
+player_one = gets.chomp
 
-if validate_name(player_1)
+if validate_name(player_one)
   puts 'Valid name.'
 else
   puts 'Sorry! Invalid name. Try again.'
   puts 'Player 1 - please enter your name: '
-  player_1 = gets.chomp
-  if validate_name(player_1)
+  player_one = gets.chomp
+  if validate_name(player_one)
     puts 'Okay, good name!'
   else
     puts "Your name is invalid again. We'll just call you 'Player 1'"
-    player_1 = 'Player 1'
+    player_one = 'Player 1'.freeze
   end
 end
 
 puts ''
 
 puts 'Player 2 - please enter your name'
-player_2 = gets.chomp
+player_two = gets.chomp
 
-if validate_name(player_2)
+if validate_name(player_two)
   puts 'Valid name.'
 else
   puts 'Sorry! Invalid name. Try again.'
   puts 'Player 2 - please enter your name: '
-  player_2 = gets.chomp
-  if validate_name(player_2)
+  player_two = gets.chomp
+  if validate_name(player_two)
     puts 'Okay, good name!'
   else
     puts "Your name is invalid again. We'll just call you 'Player 2'"
-    player_2 = 'Player 2'
+    player_two = 'Player 2'.freeze
   end
 end
 
@@ -65,11 +65,17 @@ def move(board, index, token = 'X')
   board[position] = token
 end
 
-puts "#{player_1} your default token is X "
-puts "#{player_2} your default token is O"
-puts "#{player_1} chose a number from 1 to 9 to position your Token"
+puts ''
+
+puts "#{player_one} your default token is X"
+puts "#{player_two} your default token is O"
+
+puts ''
+
+puts "#{player_one} chose a number from 1 to 9 to position your Token"
 index = gets.chomp
 move(board, index)
-display_board(board)
 
-# Ask player for move and print that on screen.
+puts ''
+
+display_board(board)
