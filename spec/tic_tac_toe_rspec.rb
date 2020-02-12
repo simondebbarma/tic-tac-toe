5d5051c8f97e9ed7b2b515064757d7d914f1b795
certilremy <certilremy@gmail.com>
2020-02-12 15:18:33 -0500

    added test for board instace variable

---
+++ spec/tic_tac_toe_rspec.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/player.rb'
+require_relative '../bin/main.rb'
+require_relative '../lib/input_validator_helper.rb'
+require_relative '../lib/menu.rb'
+require_relative '../lib/game.rb'
+
+RSpec.describe TicTacToe do
+  describe '#initialize' do
+    it ' Create an instance variable for the game with element from 1 to 9' do
+      tictactoe = TicTacToe.new
+      expect tictactoe.instance_variable_get(:@board).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    end
+  end
+end
