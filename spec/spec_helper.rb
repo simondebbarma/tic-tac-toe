9ec3c926e43cb34a8f8b8d0f290f453006762a63
certilremy <certilremy@gmail.com>
2020-02-12 16:23:35 -0500

    created the test for the display board

---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require_relative '../bin/main.rb'
+
+RSpec.configure do |config|
+end
+
+
--- spec/tic_tac_toe_rspec.rb
+++ spec/tic_tac_toe_rspec.rb
@@ -4,11 +4,34 @@ require_relative '../lib/input_validator_helper.rb'
 require_relative '../lib/menu.rb'
 require_relative '../lib/game.rb'
 
-RSpec.describe TicTacToe do
-  describe '#initialize' do
-    it ' Create an instance variable for the game with element from 1 to 9' do
-      tictactoe = TicTacToe.new
+describe TicTacToe do
+  describe 'The existence of the board' do
+    let(:tictactoe) { TicTacToe.new }
+    it ' Create an instance variable of the board for the game with element from 1 to 9' do
       expect(tictactoe.instance_variable_get(:@board)).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    end
+  end
+
+  describe ' Display the board ' do
+    let(:board) { %w[X X X O X O X O X] }
+    let(:tictactoe) { TicTacToe.new }
+    def capture_ouput
+      old_stdout = $stdout
+      $stdout = StringIO.new('', 'w')
+      yield
+      $stdout.string
+    ensure
+      $stdout = old_stdout
+    end
+
+    let(:output) { capture_output { tictactoe.show_board } }
+    it 'print the board for the player' do
+      tictactoe.instance_variable_set(:@board, board)
+      expect(output).to include(' X | X | X')
+      expect(output).to include('-----------')
+      expect(output).to include(' O | X | O')
+      expect(output).to include('-----------')
+      expect(output).to include(' X | O | X')
     end
   end
 end
