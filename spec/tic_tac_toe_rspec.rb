b84085aa3abfb33a2113592d87f144ffb6e1e96b
Simon Peter Debbarma <31207418+simonpeterdebbarma@users.noreply.github.com>
2020-02-12 14:24:04 -0600

    :bug: fix minor rspec bug and modify .rspec to accept our test file

--- spec/tic_tac_toe_rspec.rb
+++ spec/tic_tac_toe_rspec.rb
@@ -8,7 +8,7 @@ RSpec.describe TicTacToe do
   describe '#initialize' do
     it ' Create an instance variable for the game with element from 1 to 9' do
       tictactoe = TicTacToe.new
-      expect tictactoe.instance_variable_get(:@board).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      expect(tictactoe.instance_variable_get(:@board)).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9])
     end
   end
 end
